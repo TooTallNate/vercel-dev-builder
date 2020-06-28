8f8a315dc8f7b0ca8767692b9c704427db794fe4
Nathan Rajlich <nathan@tootallnate.net>
2020-06-27 19:59:01 -0700

    Prettier

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 const { join } = require('path');
 const { spawnSync } = require('child_process');
-const { download, runNpmInstall, runPackageJsonScript } = require('@vercel/build-utils');
+const {
+	download,
+	runNpmInstall,
+	runPackageJsonScript,
+} = require('@vercel/build-utils');
 
 function analyze({ files, entrypoint }) {
 	return files[entrypoint].digest;
 }
 
 async function build(opts) {
-  const { files, workPath, meta } = opts;
-  await download(files, workPath, meta);
+	const { files, workPath, meta } = opts;
+	await download(files, workPath, meta);
 
-  const installTime = Date.now();
-  console.log('Installing local runtime dependencies...');
-  await runNpmInstall(
-    workPath,
-    ['--prefer-offline'],
-    {},
-    meta
-  );
-  console.log(`Install complete [${Date.now() - installTime}ms]`);
+	const installTime = Date.now();
+	console.log('Installing local runtime dependencies...');
+	await runNpmInstall(workPath, ['--prefer-offline'], {}, meta);
+	console.log(`Install complete [${Date.now() - installTime}ms]`);
 
-  await runPackageJsonScript(workPath, 'build');
+	await runPackageJsonScript(workPath, 'build');
 
-  // TODO: purge the require cache before require()
-  const builder = require(workPath);
-  return builder.build(opts);
+	// TODO: purge the require cache before require()
+	const builder = require(workPath);
+	return builder.build(opts);
 }
 
 async function shouldServe(opts) {
-  // TODO: purge the require cache before require()
-  const builder = require(opts.workPath);
-  if (typeof builder.shouldServe === 'function') {
-    return builder.shouldServe(opts);
-  }
-  return false;
+	// TODO: purge the require cache before require()
+	const builder = require(opts.workPath);
+	if (typeof builder.shouldServe === 'function') {
+		return builder.shouldServe(opts);
+	}
+	return false;
 }
 
 async function startDevServer(opts) {
-  // TODO: purge the require cache before require()
-  const builder = require(opts.workPath);
-  if (typeof builder.startDevServer === 'function') {
-    return builder.startDevServer(opts);
-  }
-  return null;
+	// TODO: purge the require cache before require()
+	const builder = require(opts.workPath);
+	if (typeof builder.startDevServer === 'function') {
+		return builder.startDevServer(opts);
+	}
+	return null;
 }
 
 module.exports = {
-  version: 3,
-  analyze,
-  build,
-  shouldServe,
-  startDevServer,
+	version: 3,
+	analyze,
+	build,
+	shouldServe,
+	startDevServer,
 };
